972c2ed38388ef1c2f782619d0eb1a4a6b5008f5
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-30 17:59:17 +0200

    feat(step 2): import in app.js router from posts.js with /posts route prefix

---
+++ app.js
@@ -0,0 +1,30 @@
+// Define server port
+const PORT = 3001;
+
+// Express importing
+const express = require('express');
+
+// Posts router importing
+const postsRouter = require('./routers/posts');
+
+// Initialize a new express server
+const app = express();
+
+// Use imported posts router
+app.use('/posts', postsRouter);
+
+// Server root route
+app.get('/', (request, response) => {
+
+    // Log you are on server root route
+    response.send('You are on the server root route.');
+
+});
+
+// Start server listening
+app.listen(PORT, () => {
+
+    // Log server listening
+    console.log(`Server is listening on http://localhost:${PORT}...`);
+
+});
